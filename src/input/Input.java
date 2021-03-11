90ba0b4218c71ddf47dff2f7598f5efd3a464ff4
s.m.aminul islam <robinraja42@gmail.com>
2021-03-11 17:23:59 +0600

    Input

--- src/input/Input.java
+++ src/input/Input.java
@@ -1,21 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package input;
 
-/**
- *
- * @author devil
- */
+import java.util.Scanner;
+
+
 public class Input {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter a number:");
+           int a= sc.nextInt();
+           System.out.println("Enter another number:");
+           int b = sc.nextInt();
+           int result = a+b ;
+           System.out.println("The result :"+result);
     }
     
 }
